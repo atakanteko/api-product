90246e69f47c90374502e77412ccb69e3c98bb8f
Atakan Tekoğlu <atakantekoglu@gmail.com>
2023-02-26 01:43:46 +0300

    production build of front-end

---
+++ dist/assets/View-7b8a471c.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-d08e96ff.js";const t=()=>e("section",{children:e("h1",{children:"Another Page"})});export{t as default};
---
+++ dist/assets/View-9dfdd116.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-8d0e671f.js";const t=()=>e("section",{children:e("h1",{children:"Another Page"})});export{t as default};
