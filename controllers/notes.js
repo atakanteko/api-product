2c453238e5a772ee530a8798267f0b3e3d979907
Atakan Tekoğlu <atakantekoglu@gmail.com>
2023-02-28 23:35:27 +0300

    creating a new note

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
 
 notesRouter.get("/", (req, resp) => {
   Note.find({}).then((notes) => {
@@ -56,9 +57,10 @@ notesRouter.patch("/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-notesRouter.post("/", (request, response, next) => {
+notesRouter.post("/", async (request, response) => {
   const body = request.body;
-
+  const user = await User.findById(body.userId);
+  console.log("user", user);
   if (!body.content) {
     return response.status(400).json({
       error: "content missing",
@@ -67,14 +69,16 @@ notesRouter.post("/", (request, response, next) => {
 
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important === undefined ? false : body.important,
+    user: user.id,
   });
-  note
-    .save()
-    .then(() => {
-      response.json(note);
-    })
-    .catch((error) => next(error));
+
+  const savedNote = await note.save();
+  console.log(savedNote);
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
+  response.json(savedNote);
 });
 
 module.exports = notesRouter;
