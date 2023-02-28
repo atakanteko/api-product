099a40ae26fbe4779247ff9347ca3b9cb75d6c48
Atakan Tekoğlu <atakantekoglu@gmail.com>
2023-02-28 23:18:37 +0300

    creating users

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const cors = require("cors");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const notesRouter = require("./controllers/notes");
+const usersRouter = require("./controllers/users");
+
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
@@ -27,6 +29,7 @@ app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
